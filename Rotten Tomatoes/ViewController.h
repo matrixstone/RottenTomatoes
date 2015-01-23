0a2b3f12fc9509a9ea36989d5ac31fc92fedfe0b
xuhe <xuhe@yahoo-inc.com>
2015-01-22 22:27:18 -0800

    Initial Commit

---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Xu He on 1/22/15.
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Rotten Tomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rotten Tomatoes
+//
+//  Created by Xu He on 1/22/15.
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
